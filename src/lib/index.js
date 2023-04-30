bb3c0bc43d6518303f9c1c3ee4e3aae1d091e36b
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2023-04-30 11:25:32 -0400

    fix: rename index.js to index.ts

--- src/lib/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './components/button/Button.svelte';
-export { default as ButtonGroup } from './components/button/ButtonGroup.svelte';
-export { default as Group } from './components/group/Group.svelte';
