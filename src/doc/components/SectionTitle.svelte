271cfe3a0bbbd2389f7ff7d949068ce888c965d0
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2023-05-01 22:46:42 -0400

    feat: add documentation to getting-started

--- src/doc/components/SectionTitle.svelte
+++ src/doc/components/SectionTitle.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   export let title: string;
+  export let noTopMargin: boolean = false;
 </script>
 
-<a href="#{title}" id={title} class="mb-4 flex items-center gap-1 text-3xl">
-  <span class="text-xl text-base-content/25 font-semibold">#</span>
+<a
+  href="#{title}"
+  id={title}
+  class:mt-8={!noTopMargin}
+  class="mb-4 flex w-max items-center gap-1 text-3xl"
+>
+  <span class="text-xl font-semibold text-base-content/25">#</span>
   {title}
 </a>
---
+++ src/lib/components/alert/Alert.svelte
@@ -0,0 +1,59 @@
+<script context="module" lang="ts">
+  const types = {
+    default: {
+      class: '',
+      path: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+      stroke: 'stroke-info'
+    },
+    info: {
+      class: 'alert-info',
+      path: 'M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+      stroke: 'stroke-current'
+    },
+    success: {
+      class: 'alert-success',
+      path: 'M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z',
+      stroke: 'stroke-current'
+    },
+    warning: {
+      class: 'alert-warning',
+      path: 'M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z',
+      stroke: 'stroke-current'
+    },
+    error: {
+      class: 'alert-error',
+      path: 'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
+      stroke: 'stroke-current'
+    },
+  };
+
+  export type Type = keyof typeof types;
+</script>
+
+<script lang="ts">
+  import { getClasses } from '$lib/utils.js';
+
+  export let type: Type = 'default';
+
+  $: classes = getClasses(types[type].class);
+</script>
+
+<div class="alert shadow-lg {classes} {$$props.class}">
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 flex-shrink-0 {types[type].stroke}"
+      fill="none"
+      viewBox="0 0 24 24"
+      ><path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d={types[type].path}
+      /></svg
+    >
+    <span>
+      <slot />
+    </span>
+  </div>
+</div>
