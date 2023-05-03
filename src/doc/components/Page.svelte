b7b8e8fe543fecfe0979cf020f049f461d21f1a2
Mathis Côté <77208006+BenocxX@users.noreply.github.com>
2023-05-03 16:09:42 -0400

    feat: add doc for button component

--- src/doc/components/Page.svelte
+++ src/doc/components/Page.svelte
@@ -1,8 +1,8 @@
 <div class="flex gap-8">
-  <div class="w-5/6 max-w-full">
+  <div class="w-full lg:w-5/6 max-w-full">
     <slot />
   </div>
-  <div class="w-1/6 hidden lg:block">
+  <div class="lg:w-1/6 hidden lg:block">
     <slot name="sidebar" />
   </div>
 </div>
